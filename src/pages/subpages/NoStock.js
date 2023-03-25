c8e6f4e43f19ab9a59d160ecb1d1e22330fcc5bc
Junmaril Robles <junmarilr@gmail.com>
2023-03-26 00:32:41 +0800

    nostock.js added

---
+++ src/pages/subpages/NoStock.js
@@ -0,0 +1,20 @@
+import "../../App.css";
+import { useNavigate } from "react-router-dom";
+import { IoChevronBackOutline } from "react-icons/io5";
+
+export default function NoStock() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="overstock-container">
+      <div className="overstock-content-wrapper">
+        <div onClick={() => navigate(-1)} className="back-btn">
+          <IoChevronBackOutline color="#FFFFFF" size="2vw" />
+        </div>
+        <div style={{ marginLeft: "1vw" }}>
+          <h1 style={{ color: "#7E7E7E" }}>Products out of stock</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
