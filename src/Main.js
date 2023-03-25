f57340193dccbda35572cb6e3b25dfb485415c6e
Junmaril Robles <junmarilr@gmail.com>
2023-03-26 00:35:42 +0800

    dashboard link to nostock and overstock

--- src/Main.js
+++ src/Main.js
@@ -8,7 +8,6 @@ import {
   AiOutlineCalendar,
 } from "react-icons/ai";
 import {
-  BrowserRouter,
   Link,
   useMatch,
   useResolvedPath,
@@ -21,6 +20,8 @@ import Dashboard from "./pages/Dashboard";
 import Products from "./pages/Products";
 import Login from "./pages/Login";
 import ErrorPage from "./pages/ErrorPage";
+import NoStock from "./pages/subpages/NoStock";
+import OverStock from "./pages/subpages/OverStock";
 
 function Main() {
   const [contentWrapperHeight, setContentWrapperHeight] = useState(0);
@@ -42,88 +43,90 @@ function Main() {
   }, []);
 
   return (
-      <div className="canvas">
-        <div
-          className="content-wrapper"
-          style={{ height: contentWrapperHeight }}
-        >
-          <div className="nav-wrapper">
-            <div className="logo-wrapper">
-              <img src={logo} className="app-logo" alt="logo" />
-              <h1>Store.</h1>
-            </div>
-            <div className="menu-wrapper">
-              <h1>Menu</h1>
+    <div className="canvas">
+      <div className="content-wrapper" style={{ height: contentWrapperHeight }}>
+        <div className="nav-wrapper">
+          <div className="logo-wrapper">
+            <img src={logo} className="app-logo" alt="logo" />
+            <h1>Store.</h1>
+          </div>
+          <div className="menu-wrapper">
+            <h1>Menu</h1>
+            <div className="db-wrapper">
               <div className="db-wrapper">
-                <div className="db-wrapper">
-                  <div className="btn-container">
-                    <CustomLink
-                      to="/dashboard"
-                      icon={(isActive) =>
-                        isActive ? (
-                          <AiOutlineDashboard color="#47A515" size="1.5vw" />
-                        ) : (
-                          <AiOutlineDashboard color="#A6A6A6" size="1.5vw" />
-                        )
-                      }
-                    >
-                      Dashboard
-                    </CustomLink>
-                  </div>
-                  <div className="btn-container">
-                    <CustomLink
-                      to="/products"
-                      icon={(isActive) =>
-                        isActive ? (
-                          <AiOutlineBars color="#47A515" size="1.5vw" />
-                        ) : (
-                          <AiOutlineBars color="#A6A6A6" size="1.5vw" />
-                        )
-                      }
-                    >
-                      Products
-                    </CustomLink>
-                  </div>
-                  <div className="btn-container">
-                    <CustomLink
-                      to="/activities"
-                      icon={(isActive) =>
-                        isActive ? (
-                          <AiOutlineCalendar color="#47A515" size="1.5vw" />
-                        ) : (
-                          <AiOutlineCalendar color="#A6A6A6" size="1.5vw" />
-                        )
-                      }
-                    >
-                      Activities
-                    </CustomLink>
-                  </div>
+                <div
+                  style={{ flexDirection: "column" }}
+                  className="btn-container"
+                >
+                  <CustomLink
+                    to="/dashboard"
+                    icon={(isActive) =>
+                      isActive ? (
+                        <AiOutlineDashboard color="#47A515" size="1.5vw" />
+                      ) : (
+                        <AiOutlineDashboard color="#A6A6A6" size="1.5vw" />
+                      )
+                    }
+                  >
+                    Dashboard
+                  </CustomLink>
+                </div>
+                <div className="btn-container">
+                  <CustomLink
+                    to="/products"
+                    icon={(isActive) =>
+                      isActive ? (
+                        <AiOutlineBars color="#47A515" size="1.5vw" />
+                      ) : (
+                        <AiOutlineBars color="#A6A6A6" size="1.5vw" />
+                      )
+                    }
+                  >
+                    Products
+                  </CustomLink>
+                </div>
+                <div className="btn-container">
+                  <CustomLink
+                    to="/activities"
+                    icon={(isActive) =>
+                      isActive ? (
+                        <AiOutlineCalendar color="#47A515" size="1.5vw" />
+                      ) : (
+                        <AiOutlineCalendar color="#A6A6A6" size="1.5vw" />
+                      )
+                    }
+                  >
+                    Activities
+                  </CustomLink>
                 </div>
               </div>
             </div>
           </div>
-          <div className="main-wrapper">
-            <Routes>
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/activities" element={<Activities />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/error" element={<ErrorPage />} />
-              <Route path="/" element={<Navigate to="/login" replace />} />
-              <Route path="*" element={<Navigate to="/error" replace />} />
-            </Routes>
-          </div>
+        </div>
+        <div className="main-wrapper">
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/activities" element={<Activities />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/error" element={<ErrorPage />} />
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="*" element={<Navigate to="/error" replace />} />
+            <Route path="/dashboard/nostock" element={<NoStock />} />
+            <Route path="/dashboard/overstock" element={<OverStock />} />
+          </Routes>
         </div>
       </div>
+    </div>
   );
 }
 
 function CustomLink({ to, icon, children, ...props }) {
   const resolvedPath = useResolvedPath(to);
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  const isActive = useMatch({ path: resolvedPath.pathname, end: null });
 
   return (
-    <li className={isActive ? "active" : ""}>
+    <li className={ isActive ?  "active" : ""}>
       <div className="link-wrapper">
         {icon(isActive)}
         <Link to={to} {...props}>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Dashboard() {
-    return(
-        <h1>Dashboard</h1>
-    )
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div className="dashboard-cards">
+        <Link to="/dashboard/nostock">Products out of Stock</Link>
+        <Link to="/dashboard/overstock">Products overstocked</Link>
+      </div>
+    </div>
+  );
 }
