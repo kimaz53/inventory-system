bc79bd592bf42f217cadd8a0deaa8af6ad2d0440
Junmaril Robles <junmarilr@gmail.com>
2023-03-31 01:19:47 +0800

    added top bars and modified cards styling

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,15 +1,47 @@
 import "../App.css";
 import { Link } from "react-router-dom";
 import bags from "../../src/bags.png";
+import {
+  IoSearchOutline,
+  IoPersonCircleOutline,
+  IoNotificationsOutline,
+} from "react-icons/io5";
+import { useState } from "react";
 
 export default function Dashboard() {
+  const [notifData, setNotifData] = useState(0);
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <div className="dashboard-cards">
-        <Link to="/products">All Products</Link>
-        <Link to="/dashboard/nostock">Products out of Stock</Link>
-        <Link to="/dashboard/overstock">Products overstocked</Link>
+    <div className="stock-container">
+      <div
+        style={{ padding: "0vw 2vw 4vw 2vw" }}
+        className="product-nav-wrapper"
+      >
+        <div style={{ marginBottom: "-2vw" }} className="desc-nav">
+          <h1>Hello, Good Day.</h1>
+          <p>Welcome back!</p>
+        </div>
+
+        <div style={{ marginRight: "3.5vw" }} className="search-wrapper">
+          <input
+            type="text"
+            id="myInput"
+            placeholder="Search"
+            className="search-input"
+          />
+          <IoSearchOutline className="search-icon" color="#7E7E7E" size="2vw" />
+        </div>
+        <div
+          style={{ marginRight: "1vw", flexDirection: "column" }}
+          className="filter-btn"
+        >
+          {notifData > 0 ? null : <div className="notifBall"/>}
+          <IoNotificationsOutline color="#7E7E7E" size="2vw" />
+        </div>
+
+        <div className="filter-btn">
+          <IoPersonCircleOutline color="#7E7E7E" size="2vw" />
+        </div>
       </div>
 
       <div className="cards-container">
