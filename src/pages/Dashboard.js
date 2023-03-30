92d17ea8f875d6c21c114a4ec72898b06a74d3ba
Christian Babatido <christianbabatido3918@gmail.com>
2023-03-30 21:08:24 +0800

    modified dashboard, inputed prior codes and data

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,6 @@
+import "../App.css";
 import { Link } from "react-router-dom";
+import bags from "../../src/bags.png";
 
 export default function Dashboard() {
   return (
@@ -8,6 +10,65 @@ export default function Dashboard() {
         <Link to="/products">All Products</Link>
         <Link to="/dashboard/nostock">Products out of Stock</Link>
         <Link to="/dashboard/overstock">Products overstocked</Link>
+      </div>
+
+      <div className="cards-container">
+        <div className="parent-div">
+          <div className="sub-parent">
+            <div className="title-txt">
+              <p>Count of all</p>
+              <p>products</p>
+            </div>
+            <div className="data-txt">
+              <h1>123</h1>
+            </div>
+          </div>
+
+          <div className="image-and-button">
+            <img src={bags} className="bags-data-img" alt="logo" />
+            <Link className="more-products-btn" to="/products">
+              More
+            </Link>
+          </div>
+        </div>
+
+        <div className="parent-div">
+          <div className="sub-parent">
+            <div className="title-txt">
+              <p>Count of all</p>
+              <p>products</p>
+            </div>
+            <div className="data-txt">
+              <h1>123</h1>
+            </div>
+          </div>
+
+          <div className="image-and-button">
+            <img src={bags} className="bags-data-img" alt="logo" />
+            <Link className="more-products-btn" to="/products">
+              More
+            </Link>
+          </div>
+        </div>
+
+        <div className="parent-div">
+          <div className="sub-parent">
+            <div className="title-txt">
+              <p>Count of all</p>
+              <p>products</p>
+            </div>
+            <div className="data-txt">
+              <h1>123</h1>
+            </div>
+          </div>
+
+          <div className="image-and-button">
+            <img src={bags} className="bags-data-img" alt="logo" />
+            <Link className="more-products-btn" to="/products">
+              More
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
