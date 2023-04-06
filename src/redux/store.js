c48a97cb5b47ab08d48501e4f9c9c3bbf8cb8954
Junmaril Robles <junmarilr@gmail.com>
2023-04-06 10:49:08 +0800

    redux added username and password for login

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,20 @@ import { createStore } from "redux";
 
 const initialState = {
   selectedItemId: 0,
+  username: "",
+  password: "",
 };
 
 function rootReducer(state = initialState, action) {
   switch (action.type) {
     case "SET_SELECTED_ITEM_ID":
       return { ...state, selectedItemId: action.payload };
+    case "LOGIN":
+      return {
+        ...state,
+        username: action.payload.username,
+        password: action.payload.password,
+      };
     default:
       return state;
   }
