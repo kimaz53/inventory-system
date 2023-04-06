5d539f1cbafa0c68fd3ff903ad4d84c2ee5789b1
Kristian <skristian1994@gmail.com>
2023-04-06 09:18:59 +0800

    added login form. but needed style.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,6 +4,15 @@ import { Link } from "react-router-dom";
 export default function Login() {
   return (
     <div className="login-wrapper">
+      <h5>Authorized Personel only. Please sign-in first!</h5>
+      <br></br>
+      <div>
+        <label>Username: </label>
+        <input type="text" name ="uname" placeholder ="type your username here" />
+        <br></br>
+        <label>Password: </label>
+        <input type="password" name ="pword" placeholder ="type your password here" />
+      </div>
       <Link to="/dashboard">Login</Link>
     </div>
   );
