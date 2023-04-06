31409db59ebd20c461d352f5c399f1f7267e4814
Junmaril Robles <junmarilr@gmail.com>
2023-04-06 10:51:06 +0800

    if user credentials is correct then can login

--- src/Main.js
+++ src/Main.js
@@ -2,18 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import logo from "../src/logo.png";
 import "@fontsource/inter";
-import {
-  AiOutlineDashboard,
-  AiOutlineBars,
-} from "react-icons/ai";
-import {
-  Link,
-  useMatch,
-  useResolvedPath,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { AiOutlineDashboard, AiOutlineBars } from "react-icons/ai";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Activities from "./pages/Activities";
 import Dashboard from "./pages/Dashboard";
 import Products from "./pages/Products";
@@ -23,8 +13,12 @@ import NoStock from "./pages/subpages/NoStock";
 import OverStock from "./pages/subpages/OverStock";
 import { IoFileTrayOutline, IoFileTrayFullOutline } from "react-icons/io5";
 import { CiCalendar } from "react-icons/ci";
+import { useSelector } from "react-redux";
+import CustomLink from "./pages/CustomLink";
 
 function Main() {
+  const { username, password } = useSelector((state) => state);
+
   const [contentWrapperHeight, setContentWrapperHeight] = useState(0);
 
   useEffect(() => {
@@ -44,135 +38,135 @@ function Main() {
   }, []);
 
   return (
-    <div className="canvas">
-      <div className="content-wrapper" style={{ height: contentWrapperHeight }}>
-        <div className="nav-wrapper">
-          <div className="logo-wrapper">
-            <img src={logo} className="app-logo" alt="logo" />
-            <h1>Store.</h1>
-          </div>
-          <div className="menu-wrapper">
-            <h1>Menu</h1>
-            <div className="db-wrapper">
-              <div className="db-wrapper">
-                <div className="btn-container">
-                  <CustomLink
-                    to="/dashboard"
-                    icon={(isActive) =>
-                      isActive ? (
-                        <AiOutlineDashboard color="#47A515" size="1.5vw" />
-                      ) : (
-                        <AiOutlineDashboard color="#A6A6A6" size="1.5vw" />
-                      )
-                    }
-                  >
-                    Dashboard
-                  </CustomLink>
-                </div>
+    <>
+      {username === "admin" && password === "admin" ? (
+        <div className="canvas">
+          <div
+            className="content-wrapper"
+            style={{ height: contentWrapperHeight }}
+          >
+            <div className="nav-wrapper">
+              <div className="logo-wrapper">
+                <img src={logo} className="app-logo" alt="logo" />
+                <h1>Store.</h1>
+              </div>
+              <div className="menu-wrapper">
+                <h1>Menu</h1>
+                <div className="db-wrapper">
+                  <div className="db-wrapper">
+                    <div className="btn-container">
+                      <CustomLink
+                        to="/dashboard"
+                        icon={(isActive) =>
+                          isActive ? (
+                            <AiOutlineDashboard color="#47A515" size="1.5vw" />
+                          ) : (
+                            <AiOutlineDashboard color="#A6A6A6" size="1.5vw" />
+                          )
+                        }
+                      >
+                        Dashboard
+                      </CustomLink>
+                    </div>
 
-                <div className="btn-container">
-                  <CustomLink
-                    to="/products"
-                    icon={(isActive) =>
-                      isActive ? (
-                        <AiOutlineBars color="#47A515" size="1.5vw" />
-                      ) : (
-                        <AiOutlineBars color="#A6A6A6" size="1.5vw" />
-                      )
-                    }
-                  >
-                    Products
-                  </CustomLink>
-                </div>
+                    <div className="btn-container">
+                      <CustomLink
+                        to="/products"
+                        icon={(isActive) =>
+                          isActive ? (
+                            <AiOutlineBars color="#47A515" size="1.5vw" />
+                          ) : (
+                            <AiOutlineBars color="#A6A6A6" size="1.5vw" />
+                          )
+                        }
+                      >
+                        Products
+                      </CustomLink>
+                    </div>
 
-                <div>
-                  <div className="btn-container">
-                    <CustomLink
-                      to="/products/nostock"
-                      icon={(isActive) =>
-                        isActive ? (
-                          <IoFileTrayOutline color="#47A515" size="1.5vw" />
-                        ) : (
-                          <IoFileTrayOutline color="#A6A6A6" size="1.5vw" />
-                        )
-                      }
-                    >
-                      Out of Stock
-                    </CustomLink>
+                    <div>
+                      <div className="btn-container">
+                        <CustomLink
+                          to="/products/nostock"
+                          icon={(isActive) =>
+                            isActive ? (
+                              <IoFileTrayOutline color="#47A515" size="1.5vw" />
+                            ) : (
+                              <IoFileTrayOutline color="#A6A6A6" size="1.5vw" />
+                            )
+                          }
+                        >
+                          Out of Stock
+                        </CustomLink>
+                      </div>
+
+                      <div className="btn-container">
+                        <CustomLink
+                          to="/products/overstock"
+                          icon={(isActive) =>
+                            isActive ? (
+                              <IoFileTrayFullOutline
+                                color="#47A515"
+                                size="1.5vw"
+                              />
+                            ) : (
+                              <IoFileTrayFullOutline
+                                color="#A6A6A6"
+                                size="1.5vw"
+                              />
+                            )
+                          }
+                        >
+                          Overstocked
+                        </CustomLink>
+                      </div>
+                    </div>
+
+                    <div className="btn-container">
+                      <CustomLink
+                        to="/activities"
+                        icon={(isActive) =>
+                          isActive ? (
+                            <CiCalendar
+                              color="#47A515"
+                              size="1.5vw"
+                              strokeWidth={0.5}
+                            />
+                          ) : (
+                            <CiCalendar
+                              color="#A6A6A6"
+                              size="1.5vw"
+                              strokeWidth={0.5}
+                            />
+                          )
+                        }
+                      >
+                        Activities
+                      </CustomLink>
+                    </div>
                   </div>
-
-                  <div className="btn-container">
-                    <CustomLink
-                      to="/products/overstock"
-                      icon={(isActive) =>
-                        isActive ? (
-                          <IoFileTrayFullOutline color="#47A515" size="1.5vw" />
-                        ) : (
-                          <IoFileTrayFullOutline color="#A6A6A6" size="1.5vw" />
-                        )
-                      }
-                    >
-                      Overstocked
-                    </CustomLink>
-                  </div>
-                </div>
-
-                <div className="btn-container">
-                  <CustomLink
-                    to="/activities"
-                    icon={(isActive) =>
-                      isActive ? (
-                        <CiCalendar color="#47A515" size="1.5vw" strokeWidth={0.5} />
-                      ) : (
-                        <CiCalendar color="#A6A6A6" size="1.5vw" strokeWidth={0.5} />
-                      )
-                    }
-                  >
-                    Activities
-                  </CustomLink>
                 </div>
               </div>
             </div>
+            <div className="main-wrapper">
+              <Routes>
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/activities" element={<Activities />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/error" element={<ErrorPage />} />
+                <Route path="/" element={<Navigate to="/login" replace />} />
+                <Route path="*" element={<Navigate to="/error" replace />} />
+                <Route path="/products/nostock" element={<NoStock />} />
+                <Route path="/products/overstock" element={<OverStock />} />
+              </Routes>
+            </div>
           </div>
         </div>
-        <div className="main-wrapper">
-          <Routes>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/activities" element={<Activities />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/error" element={<ErrorPage />} />
-            <Route path="/" element={<Navigate to="/login" replace />} />
-            <Route path="*" element={<Navigate to="/error" replace />} />
-            <Route path="/products/nostock" element={<NoStock />} />
-            <Route path="/products/overstock" element={<OverStock />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function CustomLink({ to, icon, children, onClick, ...props }) {
-  const resolvedPath = useResolvedPath(to);
-  const isActive = useMatch({ path: resolvedPath.pathname, start: null });
-
-  const handleClick = (event) => {
-    if (onClick) {
-      onClick(event);
-    }
-  };
-
-  return (
-    <li className={isActive ? "active" : ""}>
-      <div className="link-wrapper">
-        {icon(isActive)}
-        <Link to={to} onClick={handleClick} {...props}>
-          {children}
-        </Link>
-      </div>
-      {isActive && <div className="rectangle" />}
-    </li>
+      ) : (
+        <Login />
+      )}
+    </>
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,38 @@
 import "../App.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 export default function Login() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  function handleLogin() {
+    const username = document.getElementsByName("uname")[0].value;
+    const password = document.getElementsByName("pword")[0].value;
+
+    dispatch({ type: "LOGIN", payload: { username, password } });
+
+    if (username === "admin" && password === "admin") {
+      navigate("/dashboard");
+    }
+  }
+
   return (
     <div className="login-wrapper">
       <h5>Authorized Personel only. Please sign-in first!</h5>
       <br></br>
       <div>
         <label>Username: </label>
-        <input type="text" name ="uname" placeholder ="type your username here" />
+        <input type="text" name="uname" placeholder="type your username here" />
         <br></br>
         <label>Password: </label>
-        <input type="password" name ="pword" placeholder ="type your password here" />
+        <input
+          type="password"
+          name="pword"
+          placeholder="type your password here"
+        />
       </div>
-      <Link to="/dashboard">Login</Link>
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
 }
