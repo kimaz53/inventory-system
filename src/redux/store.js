6d944ccb62559bab0f005faf39987dc9ce3c2285
Junmaril Robles <junmarilr@gmail.com>
2023-04-12 17:45:12 +0800

    added local storage to determine user auth

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore } from "redux";
 
+const storedIsLogin = localStorage.getItem("isLogin");
 const initialState = {
   selectedItemId: 0,
   username: "",
   password: "",
+  isLogin: storedIsLogin === "true",
 };
 
 function rootReducer(state = initialState, action) {
@@ -11,10 +13,25 @@ function rootReducer(state = initialState, action) {
     case "SET_SELECTED_ITEM_ID":
       return { ...state, selectedItemId: action.payload };
     case "LOGIN":
+      localStorage.setItem("isLogin", "true");
       return {
         ...state,
         username: action.payload.username,
         password: action.payload.password,
+        isLogin: true,
+      };
+    case "LOGOUT":
+      localStorage.removeItem("isLogin");
+      return {
+        ...state,
+        username: "",
+        password: "",
+        isLogin: false,
+      };
+    case "STAY":
+      return {
+        ...state,
+        isLogin: true,
       };
     default:
       return state;
